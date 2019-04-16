d9a064062a6fce8da73613daaebad0713c629a03
kevinphm <kevhphm@gmail.com>
2019-04-16 10:34:11 -0700

    final push

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
+
 module.exports = {
   entry: __dirname + '/client/src/index.jsx',
+  mode: 'production',
   module: {
     rules: [
       { 
@@ -30,7 +33,23 @@ module.exports = {
       },
     ]
   },
-  
+  plugins: [
+    new BrotliGzipPlugin({
+        asset: '[path].br[query]',
+        algorithm: 'brotli',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+        quality: 11
+    }),
+    new BrotliGzipPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    })
+],
    output: {
     filename: 'bundle.js',
     path: __dirname + '/public/dist'
